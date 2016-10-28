b142a23fb6bd9ab013805197bab665403b181d6f
Geoffrey Shmigelsky <geoff@shmigelsky.com>
2016-10-27 23:11:28 -0700

    Attachment only

--- src/app.js
+++ src/app.js
@@ -77,15 +77,13 @@ function processEvent(event) {
 //                         sendFBMessage(sender, {text: textPart}, callback);
 //                     });
 //                     async.eachSeries(splittedText, (textPart, callback) => {
-                    sendFBMessage(sender, { "message":{
-                                                "attachment":{
+                    sendFBMessage(sender, "attachment":{
                                                      "type":"image",
                                                         "payload":{
                                                              "url":"https://en.wikipedia.org/wiki/Serengeti_National_Park#/media/File:Serengeti-Landscape-2012.JPG"
                                                                   }
                                                              }
-                                                       }
-                                          } , callback);
+                                                       , callback);
                     });
     
                 }
